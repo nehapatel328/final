34bd5bb7d9ad7851a077a334b94122f84b0611d5
Neha Patel <nehapatel0393@gmail.com>
2019-08-29 22:06:48 -0500

    struggling with pulling data through props

--- pages/grid.vue
+++ pages/grid.vue
@@ -1,5 +1,6 @@
 <template>
-<div class="container" id="app">
+<div id="app">
+<div class="container">
   <div class="row">
     <div class="col-md-auto py-3 px-lg-5 border bg-light row align-items-center mr-auto">
       <b-card no-body class="overflow-hidden" style="max-width: 540px;">
@@ -11,11 +12,10 @@
           <b-col md="6">
             <b-card-body title="Ted Chiang">
               <b-card-text>
-                <ul>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
+                <ul id="listOne" class="demo">
+                <li v-for="value in object">
+                  {{list}}
+                </li>
                 </ul>
               </b-card-text>
             </b-card-body>
@@ -28,17 +28,18 @@
       <b-card no-body class="overflow-hidden" style="max-width: 540px;">
         <b-row no-gutters>
           <b-col md="6">
-            <b-card-img src="~/assets/booksix.jpg" class="img-fluid rounded-0" alt="bookshelves"></b-card-img>
+            <b-card-img src="~/assets/booksix.jpg" class="img-fluid rounded-0" alt="bookshelve"></b-card-img>
             <!--Image link:https://flic.kr/p/qef5tT-->
           </b-col>
           <b-col md="6">
             <b-card-body title="Jhumpa Lahiri">
               <b-card-text>
                 <ul>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
+                <li>{{ title }}</li>
+                <li>{{ publisher }}</li>
+                <li>{{ ranks_history }}</li>
+                <li>{{ isbns }}</li>
+                <li>{{ description }}</li>
                 </ul>
               </b-card-text>
             </b-card-body>
@@ -58,14 +59,15 @@
             <!--Image link:https://flic.kr/p/Cm9pC-->
           </b-col>
           <b-col md="6">
-            <b-card-body title="Jane Austen">
+            <b-card-body title="Stephen King">
               <b-card-text>
-                <ul>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                </ul>
+              <ul>
+              <li>{{ title }}</li>
+              <li>{{ publisher }}</li>
+              <li>{{ ranks_history }}</li>
+              <li>{{ isbns }}</li>
+              <li>{{ description }}</li>
+              </ul>
               </b-card-text>
             </b-card-body>
           </b-col>
@@ -83,12 +85,13 @@
           <b-col md="6">
             <b-card-body title="Toni Morrison">
               <b-card-text>
-                <ul>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                </ul>
+              <ul>
+              <li>{{ title }}</li>
+              <li>{{ publisher }}</li>
+              <li>{{ ranks_history }}</li>
+              <li>{{ isbns }}</li>
+              <li>{{ description }}</li>
+              </ul>
               </b-card-text>
             </b-card-body>
           </b-col>
@@ -99,30 +102,27 @@
   <!--close row-->
 
 </div>
+</div>
 <!--close container-->
 </template>
 
 <script>
-export default {
-  head: {
-    titleTemplate: '%s - Nuxt.js',
-    meta: [{
-        charset: 'utf-8'
-      },
-      {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1'
-      },
+const url = 'https://api.nytimes.com/svc/books/v3'
+import axios from "axios";
 
-      // hid is used as unique identifier. Do not use `vmid` for it as it will not work
-      {
-        hid: 'description',
-        name: 'description',
-        content: 'Meta description'
-      }
-    ]
+new Vue({
+  el: '#listOne',
+  data: {
+    results: []
+  },
+  mounted() {
+    axios.get('https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?author=Ted%20Chiang&api-key=omt1RPMVtLZFNDcPmJZ4kL2c3xwtL1IB').then(response => {
+      this.title = response.data
+    })
   }
-}
+});
+
+
 </script>
 
 <style lang="scss">
--- pages/images.vue
+++ pages/images.vue
@@ -38,6 +38,7 @@
 </template>
 
 <script>
+
 </script>
 
 <style lang="scss">
