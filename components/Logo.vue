d553e7d8413540f22c6d408482169217464fab5c
Neha Patel <nehapatel0393@gmail.com>
2019-09-05 23:08:46 -0500

    logo.vue

--- components/Logo.vue
+++ components/Logo.vue
@@ -18,50 +18,42 @@
 </template>
 
 <script>
-export default {
+export default ({
 
-  head: {
-    title: process.env.npm_package_name || '',
-    props: ['title'],
-    meta: [{
-        charset: 'utf-8'
-      },
-      {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1'
-      },
-      {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || ''
-      }
-    ],
-    link: [{
-      rel: 'icon',
-      type: 'image/x-icon',
-      href: '/favicon.ico'
-    }],
-    script: [{
-        src: 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js'
-      },
-      {
-        src: 'https://code.jquery.com/jquery-3.3.1.slim.min.js'
-      },
-      {
-        src: 'https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js'
-      }
-    ]
-  },
+  el: '#app',
 
-  data() {
-    return {
-      info: null,
-      loading: true,
-      errored: false
-    }
-  },
-}
-</script>
+    head: {
+      title: process.env.npm_package_name || '',
+      props: ['title'],
+      meta: [{
+          charset: 'utf-8'
+        },
+        {
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1'
+        },
+        {
+          hid: 'description',
+          name: 'description',
+          content: process.env.npm_package_description || ''
+        }
+      ],
+      link: [{
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/favicon.ico'
+      }],
+      script: [{
+          src: 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js'
+        },
+        {
+          src: 'https://code.jquery.com/jquery-3.3.1.slim.min.js'
+        },
+        {
+          src: 'https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js'
+        }
+      ]
+    },
 
 <style>
 
